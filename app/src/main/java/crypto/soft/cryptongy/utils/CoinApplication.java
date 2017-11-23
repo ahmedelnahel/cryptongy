a478f366dacddd09cdfbb3f6da3d4d1605ae3657
ahmedelnahel <ahmed.fayed@gmail.com>
2017-11-23 16:41:19 +0400

    Merge remote-tracking branch 'origin/master'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    comit

--- app/src/main/java/crypto/soft/cryptongy/utils/CoinApplication.java
+++ app/src/main/java/crypto/soft/cryptongy/utils/CoinApplication.java
@@ -10,6 +10,20 @@ import io.realm.RealmConfiguration;
  */
 
 public class CoinApplication extends Application {
+
+
+    double usdt_btc = 100;
+
+    public double getUsdt_btc()
+    {
+        return usdt_btc;
+    }
+
+    public void setUsdt_btc(double usdt_btc)
+    {
+        this.usdt_btc = usdt_btc;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
