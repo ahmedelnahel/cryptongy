e7e86d2dc0ca0c7239fa5cb4051a3575347ced83
ahmedelnahel <ahmed.fayed@gmail.com>
2017-12-12 22:10:07 +0400

    Merge branch 'master' of https://github.com/ahmedelnahel/cryptongy
    # Conflicts:
    #	app/src/main/java/crypto/soft/cryptongy/feature/trade/conditional/ConditionalService.java
    
    fix alert notification

--- app/src/main/java/crypto/soft/cryptongy/feature/alert/AlertService.java
+++ app/src/main/java/crypto/soft/cryptongy/feature/alert/AlertService.java
@@ -50,22 +50,22 @@ public class AlertService extends Service {
         BittrexServices Tickerservices = new BittrexServices();
 
         for (CoinInfo coinInfo : list) {
+            boolean met = false;
             try {
                 Ticker ticker = Tickerservices.getTicker(coinInfo.getCoinName());
                 if (coinInfo.isHigher() && ticker.getResult().getLast().doubleValue() >= coinInfo.getHighValue().doubleValue() ) {
                     showNotification("Alert", coinInfo.CoinName + " is above " + String.format("%.8f", coinInfo.getHighValue().doubleValue()), 0);
-                    if (coinInfo.getAlarmFreq() == 1) {
-                        coinInfo.status = "Closed";
-                        update(coinInfo);
-                    }
+                    met = true;
                 }
 
                 if (coinInfo.isLower() && ticker.getResult().getLast().doubleValue() <= coinInfo.getLowValue().doubleValue() ) {
                     showNotification("Alert", coinInfo.CoinName + " is below " + String.format("%.8f", coinInfo.getLowValue().doubleValue()), 0);
-                    if (coinInfo.getAlarmFreq() == 1) {
-                        coinInfo.status = "Closed";
-                        update(coinInfo);
-                    }
+                   met = true;
+                }
+
+                if (met && coinInfo.getAlarmFreq() == 1) {
+                    coinInfo.status = "Closed";
+                    update(coinInfo);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
