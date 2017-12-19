03f1887432fe1806ef8d662b5a337a77f9ab5eac
ahmedelnahel <ahmed.fayed@gmail.com>
2017-12-19 20:54:24 +0400

    Merge branch 'master' of https://github.com/ahmedelnahel/cryptongy
    # Conflicts:
    #	app/src/main/java/crypto/soft/cryptongy/feature/order/OrderPresenter.java
    fix conditional error

---
+++ app/src/main/java/crypto/soft/cryptongy/feature/shared/json/order/Order.java
@@ -0,0 +1,70 @@
+
+package crypto.soft.cryptongy.feature.shared.json.order;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "success",
+    "message",
+    "result"
+})
+public class Order {
+
+    @JsonProperty("success")
+    private Boolean success;
+    @JsonProperty("message")
+    private String message;
+    @JsonProperty("result")
+    private Result result;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("success")
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    @JsonProperty("success")
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    @JsonProperty("message")
+    public String getMessage() {
+        return message;
+    }
+
+    @JsonProperty("message")
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @JsonProperty("result")
+    public Result getResult() {
+        return result;
+    }
+
+    @JsonProperty("result")
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ app/src/main/java/crypto/soft/cryptongy/feature/shared/json/order/Result.java
@@ -0,0 +1,330 @@
+
+package crypto.soft.cryptongy.feature.shared.json.order;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "AccountId",
+    "OrderUuid",
+    "Exchange",
+    "Type",
+    "Quantity",
+    "QuantityRemaining",
+    "Limit",
+    "Reserved",
+    "ReserveRemaining",
+    "CommissionReserved",
+    "CommissionReserveRemaining",
+    "CommissionPaid",
+    "Price",
+    "PricePerUnit",
+    "Opened",
+    "Closed",
+    "IsOpen",
+    "Sentinel",
+    "CancelInitiated",
+    "ImmediateOrCancel",
+    "IsConditional",
+    "Condition",
+    "ConditionTarget"
+})
+public class Result {
+
+    @JsonProperty("AccountId")
+    private Object accountId;
+    @JsonProperty("OrderUuid")
+    private String orderUuid;
+    @JsonProperty("Exchange")
+    private String exchange;
+    @JsonProperty("Type")
+    private String type;
+    @JsonProperty("Quantity")
+    private Double quantity;
+    @JsonProperty("QuantityRemaining")
+    private Double quantityRemaining;
+    @JsonProperty("Limit")
+    private Double limit;
+    @JsonProperty("Reserved")
+    private Double reserved;
+    @JsonProperty("ReserveRemaining")
+    private Double reserveRemaining;
+    @JsonProperty("CommissionReserved")
+    private Double commissionReserved;
+    @JsonProperty("CommissionReserveRemaining")
+    private Double commissionReserveRemaining;
+    @JsonProperty("CommissionPaid")
+    private Double commissionPaid;
+    @JsonProperty("Price")
+    private Double price;
+    @JsonProperty("PricePerUnit")
+    private Object pricePerUnit;
+    @JsonProperty("Opened")
+    private String opened;
+    @JsonProperty("Closed")
+    private Object closed;
+    @JsonProperty("IsOpen")
+    private Boolean isOpen;
+    @JsonProperty("Sentinel")
+    private String sentinel;
+    @JsonProperty("CancelInitiated")
+    private Boolean cancelInitiated;
+    @JsonProperty("ImmediateOrCancel")
+    private Boolean immediateOrCancel;
+    @JsonProperty("IsConditional")
+    private Boolean isConditional;
+    @JsonProperty("Condition")
+    private String condition;
+    @JsonProperty("ConditionTarget")
+    private Object conditionTarget;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("AccountId")
+    public Object getAccountId() {
+        return accountId;
+    }
+
+    @JsonProperty("AccountId")
+    public void setAccountId(Object accountId) {
+        this.accountId = accountId;
+    }
+
+    @JsonProperty("OrderUuid")
+    public String getOrderUuid() {
+        return orderUuid;
+    }
+
+    @JsonProperty("OrderUuid")
+    public void setOrderUuid(String orderUuid) {
+        this.orderUuid = orderUuid;
+    }
+
+    @JsonProperty("Exchange")
+    public String getExchange() {
+        return exchange;
+    }
+
+    @JsonProperty("Exchange")
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+
+    @JsonProperty("Type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("Type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("Quantity")
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    @JsonProperty("Quantity")
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    @JsonProperty("QuantityRemaining")
+    public Double getQuantityRemaining() {
+        return quantityRemaining;
+    }
+
+    @JsonProperty("QuantityRemaining")
+    public void setQuantityRemaining(Double quantityRemaining) {
+        this.quantityRemaining = quantityRemaining;
+    }
+
+    @JsonProperty("Limit")
+    public Double getLimit() {
+        return limit;
+    }
+
+    @JsonProperty("Limit")
+    public void setLimit(Double limit) {
+        this.limit = limit;
+    }
+
+    @JsonProperty("Reserved")
+    public Double getReserved() {
+        return reserved;
+    }
+
+    @JsonProperty("Reserved")
+    public void setReserved(Double reserved) {
+        this.reserved = reserved;
+    }
+
+    @JsonProperty("ReserveRemaining")
+    public Double getReserveRemaining() {
+        return reserveRemaining;
+    }
+
+    @JsonProperty("ReserveRemaining")
+    public void setReserveRemaining(Double reserveRemaining) {
+        this.reserveRemaining = reserveRemaining;
+    }
+
+    @JsonProperty("CommissionReserved")
+    public Double getCommissionReserved() {
+        return commissionReserved;
+    }
+
+    @JsonProperty("CommissionReserved")
+    public void setCommissionReserved(Double commissionReserved) {
+        this.commissionReserved = commissionReserved;
+    }
+
+    @JsonProperty("CommissionReserveRemaining")
+    public Double getCommissionReserveRemaining() {
+        return commissionReserveRemaining;
+    }
+
+    @JsonProperty("CommissionReserveRemaining")
+    public void setCommissionReserveRemaining(Double commissionReserveRemaining) {
+        this.commissionReserveRemaining = commissionReserveRemaining;
+    }
+
+    @JsonProperty("CommissionPaid")
+    public Double getCommissionPaid() {
+        return commissionPaid;
+    }
+
+    @JsonProperty("CommissionPaid")
+    public void setCommissionPaid(Double commissionPaid) {
+        this.commissionPaid = commissionPaid;
+    }
+
+    @JsonProperty("Price")
+    public Double getPrice() {
+        return price;
+    }
+
+    @JsonProperty("Price")
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @JsonProperty("PricePerUnit")
+    public Object getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    @JsonProperty("PricePerUnit")
+    public void setPricePerUnit(Object pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    @JsonProperty("Opened")
+    public String getOpened() {
+        return opened;
+    }
+
+    @JsonProperty("Opened")
+    public void setOpened(String opened) {
+        this.opened = opened;
+    }
+
+    @JsonProperty("Closed")
+    public Object getClosed() {
+        return closed;
+    }
+
+    @JsonProperty("Closed")
+    public void setClosed(Object closed) {
+        this.closed = closed;
+    }
+
+    @JsonProperty("IsOpen")
+    public Boolean getIsOpen() {
+        return isOpen;
+    }
+
+    @JsonProperty("IsOpen")
+    public void setIsOpen(Boolean isOpen) {
+        this.isOpen = isOpen;
+    }
+
+    @JsonProperty("Sentinel")
+    public String getSentinel() {
+        return sentinel;
+    }
+
+    @JsonProperty("Sentinel")
+    public void setSentinel(String sentinel) {
+        this.sentinel = sentinel;
+    }
+
+    @JsonProperty("CancelInitiated")
+    public Boolean getCancelInitiated() {
+        return cancelInitiated;
+    }
+
+    @JsonProperty("CancelInitiated")
+    public void setCancelInitiated(Boolean cancelInitiated) {
+        this.cancelInitiated = cancelInitiated;
+    }
+
+    @JsonProperty("ImmediateOrCancel")
+    public Boolean getImmediateOrCancel() {
+        return immediateOrCancel;
+    }
+
+    @JsonProperty("ImmediateOrCancel")
+    public void setImmediateOrCancel(Boolean immediateOrCancel) {
+        this.immediateOrCancel = immediateOrCancel;
+    }
+
+    @JsonProperty("IsConditional")
+    public Boolean getIsConditional() {
+        return isConditional;
+    }
+
+    @JsonProperty("IsConditional")
+    public void setIsConditional(Boolean isConditional) {
+        this.isConditional = isConditional;
+    }
+
+    @JsonProperty("Condition")
+    public String getCondition() {
+        return condition;
+    }
+
+    @JsonProperty("Condition")
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    @JsonProperty("ConditionTarget")
+    public Object getConditionTarget() {
+        return conditionTarget;
+    }
+
+    @JsonProperty("ConditionTarget")
+    public void setConditionTarget(Object conditionTarget) {
+        this.conditionTarget = conditionTarget;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
